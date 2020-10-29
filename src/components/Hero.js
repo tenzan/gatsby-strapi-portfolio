7358d480016d3309702cb55f79f5b729abeef120
Askar Karasaev <askar.karasaev@gmail.com>
2020-10-29 18:32:58 +0900

    80 Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,45 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
+
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      id
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>i'm askar</h1>
+            <h4>web developer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <SocialLinks></SocialLinks>
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img"></Image>
+      </div>
+    </header>
+  )
 }
 
 export default Hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,10 @@ import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 
 export default () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <Hero></Hero>
+    </Layout>
+  )
 }
 // ...GatsbyImageSharpFluid
