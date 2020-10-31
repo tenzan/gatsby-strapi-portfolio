54b4287833b70642ac6ab3e79c30b8fca5fb3039
Askar Karasaev <askar.karasaev@gmail.com>
2020-11-01 01:42:16 +0900

    100 Blog Query

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,5 +47,23 @@ export const query = graphql`
         }
       }
     }
+    allStrapiBlogs(sort: { fields: date, order: DESC }, limit: 3) {
+      nodes {
+        slug
+        content
+        desc
+        date(formatString: "MMMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
   }
 `
