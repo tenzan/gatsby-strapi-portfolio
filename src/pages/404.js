2f9a08159ac04b5ab95e74833d8a2533049e8eb4
Askar Karasaev <askar.karasaev@gmail.com>
2020-11-04 15:31:59 +0900

    114 Error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,21 @@
-import React from "react"
-import Layout from "../components/Layout"
-import { Link } from "gatsby"
-import SEO from "../components/SEO"
+import React from "react";
+import Layout from "../components/Layout";
+import { Link } from "gatsby";
+import SEO from "../components/SEO";
 
 const Error = () => {
-  return <h2>error page</h2>
-}
+  return (
+    <Layout>
+      <main className="error-page">
+        <div className="error-container">
+          <h1>Oops, this page doesn't exist</h1>
+          <Link to="/" className="btn">
+            Back home
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  );
+};
 
-export default Error
+export default Error;
